00c379d6e1e6a36427cadbd83abd2c9863a0335c
Ashish-Bridgelabz <asaschoudhary49@gmail.com>
2020-03-20 00:23:30 +0530

    MODIFIED:Finded store all the result in a dictionary

--- problemStart/arithmetic.sh
+++ problemStart/arithmetic.sh
@@ -8,11 +8,19 @@ echo "Enter the second input"
 read b
 echo "Enter the third input"
 read c
+declare -A arithmeticDictionary
+#COMPUTE THE NUMBER AND STORE THE DICTIONARY
 compute=`echo "scale=2 ; $a+$b*$c" | bc -l`
-echo $compute
+arithmeticDictionary[1]="$compute"
 compute1=`echo "scale=2 ; $a*$b+$c" | bc -l`
-echo $compute1
-compute2=`echo "scale=2 ; $c+$a/$b" | bc -l`
-echo $compute2
-compute3=1`echo "scale=2 ; $a%$b+$c" | bc -l`
-echo $compute3
+arithmeticDictionary[2]="$compute1"
+compute2=`echo "scale=2 ; $a+$b/$c" | bc -l`
+arithmeticDictionary[3]="$compute2"
+compute3=`echo "scale=2 ; $a%$b+$c" | bc -l`
+arithmeticDictionary[4]="$compute3"
+#STORE THE ELEMENT OF DICTIONARY IN ARRAY
+for value in ${!arithmeticDictionary[@]}
+do 
+	 echo arithmeticArray[$value]="${arithmeticDictionary[$value]}"
+done
+
