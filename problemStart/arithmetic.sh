2b87374942e61e5206d7386b94919208a15f1dcc
Ashish-Bridgelabz <asaschoudhary49@gmail.com>
2020-03-20 03:09:26 +0530

    MODIFIED:Finded store all the result in a descending order

--- problemStart/arithmetic.sh
+++ problemStart/arithmetic.sh
@@ -1,14 +1,15 @@
 #!/bin/bash -x
-#DISPLAY THE WELCOME STATEMENT
-echo "Welcome to the Arithmetic Computation Sorting"
-#TAKING THREE INPUT FROM USER
+##DISPLAY THE WELCOME STATEMENT
+echo "Welcome the Arithmetic Operation"
+#Display the three input
 echo "Enter the first input"
 read a
 echo "Enter the second input"
 read b
 echo "Enter the third input"
 read c
-declare -A arithmeticDictionary
+#ALL THE ARITHMETIC OPERATION STORE IN A DICTIONARY 
+declare -a arithmeticDictionary
 #COMPUTE THE NUMBER AND STORE THE DICTIONARY
 compute=`echo "scale=2 ; $a+$b*$c" | bc -l`
 arithmeticDictionary[1]="$compute"
@@ -18,9 +19,24 @@ compute2=`echo "scale=2 ; $a+$b/$c" | bc -l`
 arithmeticDictionary[3]="$compute2"
 compute3=`echo "scale=2 ; $a%$b+$c" | bc -l`
 arithmeticDictionary[4]="$compute3"
-#STORE THE ELEMENT OF DICTIONARY IN ARRAY
 for value in ${!arithmeticDictionary[@]}
-do 
-	 echo arithmeticArray[$value]="${arithmeticDictionary[$value]}"
+do
+	arithmeticArray[$value]="${arithmeticDictionary[$value]}"
 done
+#STORE ALL THE VALUE IN ARRAY 
+echo ${arithmeticArray[@]}
+#USING LOOP FOR SORTING
+for((count=1;count<=4;count++))
+do
+	for((count1=$((count+1));count1<=4;count1++))
+	do
+		if(( `echo "${arithmeticArray[$count]}<${arithmeticArray[$count1]}" | bc -q` == 1)) 
+	then
+			temp="${arithmeticArray[$count]}"
+			arithmeticArray[$count]="${arithmeticArray[$count1]}"
+			arithmeticArray[$count1]=$temp
+ 	fi
+	done
+done
+echo "sortedArray in decending order:"${arithmeticArray[@]}
 
