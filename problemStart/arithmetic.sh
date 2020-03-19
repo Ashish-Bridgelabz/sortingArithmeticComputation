9c60fe1d80c0c92ccc1c6ddbba01604268adfe05
Ashish-Bridgelabz <asaschoudhary49@gmail.com>
2020-03-19 18:56:50 +0530

    MODIFIED:Displayed three input

--- problemStart/arithmetic.sh
+++ problemStart/arithmetic.sh
@@ -1,4 +1,12 @@
 #!/bin/bash -x
 #DISPLAY THE WELCOME STATEMENT
 echo "Welcome to the Arithmetic Computation Sorting"
+#TAKING THREE INPUT FROM USER
+echo "Enter the first input"
+read a
+echo "Enter the second input"
+read b
+echo "Enter the third input"
+read c
+
 
